f27ed6d6cb0348ec12b045b84ed4876226a07199
Giovanni Vergine <verginegiovanni@gmail.com>
2024-08-13 13:11:07 +0300

    Added versioning

---
+++ libdlt/rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+[ -d build_debug  ] && rm -r build_debug
+[ -d build_release ] && rm -r build_release
+
+echo "*** Building Debug"
+mkdir build_debug && cd build_debug && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && make test && ctest -T memcheck && make libdlt_coverage && cd ..
+
+echo "*** Building Release"
+mkdir build_release && cd build_release && cmake -DCMAKE_BUILD_TYPE=Release .. && make && make test && ctest -T memcheck && cd ..
+
+echo "All done"
